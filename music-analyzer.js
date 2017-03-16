bbbf2241127aba6e443b971130d5d8256fe6f13a
Brandon Liu <bliu7654@gmail.com>
2017-03-16 01:02:02 -0700

    another test

--- music-analyzer.js
+++ music-analyzer.js
@@ -155,8 +155,8 @@ function mat3Scale(scaleSize) {
 
 function drawBeatShape(context) {
     var locMatrix = mat4();
-    locMatrix = mult(locMatrix, translation(-.5, -.2, -67.56));
-    console.log(-67.56);
+    locMatrix = mult(locMatrix, translation(-.5, -.2, -68.06));
+    console.log(-68.06);
     //If staring 60 seconds in...
     //locMatrix = mult(locMatrix, translation(-.5, -.2, -2.16));
     var mat = new Material(Color(1, 1, 1, 1), .4, .9, .9, 40);
